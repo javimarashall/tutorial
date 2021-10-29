c5b719a1ad224f41f3ad3d81e520802bb98e34a1
javimarashall <jamondragon@ucdavis.edu>
2021-10-29 13:54:30 -0700

    finish the middle section of the header

--- spanishapp/components/Header.js
+++ spanishapp/components/Header.js
@@ -19,7 +19,7 @@ import HeaderIcon from './Headericon';
 
 function Header() {
     return (
-        <div>
+        <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
 
             {/* Left */}
             <div className='flex items-center'>
@@ -30,17 +30,17 @@ function Header() {
                 />
                 <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                     <SearchIcon className='h-6 text-gray-600' />
-                    <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type='text' placeholder="Search" />
+                    <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type='text' placeholder="Search" />
                 </div>
             </div>
             {/*  Center */}
             <div className='flex justify-center flex-grow'>
                 <div className='flex space-x-6 md:space-x-2'>
-                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon active Icon={HomeIcon} />
                     <HeaderIcon Icon={FlagIcon} />
                     <HeaderIcon Icon={PlayIcon} />
                     <HeaderIcon Icon={ShoppingCartIcon} />
-                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
             {/* Right */}
